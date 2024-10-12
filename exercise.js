6d9abc9fcee1756c62115b4eef31ee61654c3540
Bikram Phurumbo <bikramphurumbo@Mangnewas-MacBook-Pro-3.local>
2024-10-11 20:19:04 -0400

    excercise1

--- exercise.js
+++ exercise.js
@@ -11,3 +11,23 @@ a = b;
 b = c; 
 console.log(b);
 console.log(a);
+// excercise 1
+// declare a function, set a name & set the values to a, and b.
+// if statement : if a > b, return a -- else, return b
+//simple way
+let number = max(50, 10);
+console.log(number);
+
+function max(a, b) {
+    if (a > b) return a;
+    return b;
+
+    
+}
+
+// optimized way
+
+function max(a, b) {
+    return (a > b) ? a : b;   
+}
+
